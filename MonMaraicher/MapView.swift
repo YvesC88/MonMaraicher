9ba58f364a5a245591d9c33d6426a1d9d0215795
YvesC88 <charpentier.yves@live.fr>
2024-02-22 12:33:51 +0100

    set swiftUI view for start project

---
+++ MonMaraicher/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  MonMaraicher
+//
+//  Created by Yves Charpentier on 22/02/2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    var body: some View {
+        
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    MapView()
+}
