f52c09089f011ca518d48d6e36118d35c3fb9f03
YvesC88 <charpentier.yves@live.fr>
2024-02-22 11:35:37 +0100

    set os version and remove storyboard

---
+++ MonMaraicher/MonMaraicher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonMaraicher
+//
+//  Created by Yves Charpentier on 22/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
