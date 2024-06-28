aa3d98760074c6ccd9826ca64adfcc601b698e8e
YvesC88 <charpentier.yves@live.fr>
2024-06-28 12:02:11 +0200

    configure Analytics

--- MonMaraicher/MonMaraicherApp.swift
+++ MonMaraicher/MonMaraicherApp.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct MonMaraicherApp: App {
-
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @AppStorage("hasCompletedOnBoarding") var hasCompletedOnBoarding: Bool = false
 
     var body: some Scene {
